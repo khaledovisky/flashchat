52a4e1b476a2211adf59418b6860802a2fcfbdd2
black <khaledhamdy2527@gmail.com>
2022-05-23 23:34:41 +0200

    finished

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _fireStore = FirebaseFirestore.instance;
+User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
+  static String id = 'ChatScreen';
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextEditingController = TextEditingController();
+  String messageText;
+
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDetail();
+  }
+
+  void getUserDetail()  {
+    try {
+      final createdUser =  _auth.currentUser;
+      if (createdUser != null) {
+        loggedInUser = createdUser;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +44,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -27,6 +57,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreambuilderClass(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -34,15 +65,21 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextEditingController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
-                  FlatButton(
+                  TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      messageTextEditingController.clear();
+                      _fireStore.collection('messages').add({
+                        'sender': loggedInUser.email,
+                        'text': messageText,
+                        'time': FieldValue.serverTimestamp() //add this
+                      });
                     },
                     child: Text(
                       'Send',
@@ -54,6 +91,96 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class StreambuilderClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _fireStore
+            .collection('messages')
+            .orderBy('time', descending: false)//add this
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blueAccent,
+              ),
+            );
+          }
+          final messages = snapshot.data.docs.reversed;
+          List<MessageBubble> messageBubbles = [];
+          for (var message in messages) {
+            final messageText = message['text'];
+            final messageSender = message['sender'];
+            final messageTime = message ['time'] as Timestamp; //add this
+            final currentUser = loggedInUser.email;
+
+            final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+              isMe: currentUser == messageSender,
+              time: messageTime, //add this
+            );
+
+            messageBubbles.add(messageBubble);
+          }
+
+          return Expanded(
+            child: ListView(
+                reverse: true,
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                children: messageBubbles),
+          );
+        });
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final String text;
+  final String sender;
+  final bool isMe;
+  final Timestamp time; // add this
+
+  MessageBubble({this.sender, this.text, this.isMe, Timestamp time}):time = time ?? Timestamp.now();//important
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+        isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            ' $sender ${DateTime.fromMillisecondsSinceEpoch(time.seconds * 1000)}',// add this only if you want to show the time along with the email. If you dont want this then don't add this DateTime thing
+            style: TextStyle(color: Colors.black54, fontSize: 12),
+          ),
+          Material(
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            borderRadius: isMe
+                ? BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30))
+                : BorderRadius.only(
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)),
+            elevation: 6,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Text(
+                text,
+                style: TextStyle(
+                    fontSize: 20, color: isMe ? Colors.white : Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
